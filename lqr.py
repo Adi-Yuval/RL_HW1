de346bc64e351eadde501ca4641afc29484a7bfc
yuvalgos <yuvalgos@gmail.com>
2022-04-26 09:59:58 +0300

    solved LQR basic

--- lqr.py
+++ lqr.py
@@ -14,7 +14,10 @@ def get_A(cart_pole_env):
     pole_length = cart_pole_env.length
     dt = cart_pole_env.tau
 
-    return np.matrix([[0]])
+    return np.eye(4) + dt * np.matrix([[0, 1, 0, 0],
+                                       [0, 0, g * pole_mass / cart_mass, 0],
+                                       [0, 0, 0, 1],
+                                       [0, 0, (g / pole_length) * (1 + pole_mass / cart_mass), 0]])
 
 
 def get_B(cart_pole_env):
@@ -29,7 +32,10 @@ def get_B(cart_pole_env):
     pole_length = cart_pole_env.length
     dt = cart_pole_env.tau
 
-    return np.matrix([[0]])
+    return np.matrix([[0],
+                      [1 / cart_mass],
+                      [0],
+                      [1 / (cart_mass * pole_length)]]) * dt
 
 
 def find_lqr_control_input(cart_pole_env):
@@ -48,21 +54,30 @@ def find_lqr_control_input(cart_pole_env):
 
     # TODO - Q and R should not be zero, find values that work, hint: all the values can be <= 1.0
     Q = np.matrix([
+        [0.05, 0, 0, 0],
         [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
+        [0, 0, 1, 0],
         [0, 0, 0, 0]
     ])
 
-    R = np.matrix([0])
+    R = np.matrix([0.001])
+
+    T = cart_pole_env.planning_steps
 
     # TODO - you need to compute these matrices in your solution, but these are not returned.
-    Ps = []
+    Ps = [Q]
 
     # TODO - these should be returned see documentation above
     us = []
     xs = [np.expand_dims(cart_pole_env.state, 1)]
     Ks = []
+
+    for i in range(T - 1, -1, -1):
+        Ps.append(Q + A.T @ Ps[-1] @ A - A.T @ Ps[-1] @ B @ np.linalg.inv(R + B.T @ Ps[-1] @ B) @ B.T @ Ps[-1] @ A)
+        Ks.append(-np.linalg.inv(R + B.T @ Ps[-1] @ B) @ B.T @ Ps[-1] @ A)
+        us.append(Ks[-1] @ xs[-1])
+
+        xs.append(A @ xs[-1] + B @ us[-1])
 
     assert len(xs) == cart_pole_env.planning_steps + 1, "if you plan for x states there should be X+1 states here"
     assert len(us) == cart_pole_env.planning_steps, "if you plan for x states there should be X actions here"
@@ -112,7 +127,7 @@ if __name__ == '__main__':
         # make action in range
         actual_action = max(env.action_space.low.item(0), min(env.action_space.high.item(0), actual_action))
         actual_action = np.array([actual_action])
-        actual_state, reward, is_done, _ = env.step(actual_action)
+        actual_state, reward, is_done, _ = env.step(np.array(actual_action, dtype=np.float32))
         is_stable = reward == 1.0
         is_stable_all.append(is_stable)
         env.render()
@@ -122,4 +137,3 @@ if __name__ == '__main__':
     valid_episode = np.all(is_stable_all[-100:])
     # print if LQR succeeded
     print('valid episode: {}'.format(valid_episode))
-
